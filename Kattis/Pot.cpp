db1dcebe421644f3fe2bbf4c45068cb978bc4bdd
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-21 17:46:10 +0530

    Add files via upload

---
+++ Kattis/Pot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int calculate(int N)
+{
+    int r=N%10;
+    N=N/10;
+    return int(pow(N,r));
+}
+int main(void)
+{
+    int t;
+    cin >> t;
+    int sum=0;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        sum+=calculate(N);
+    }
+    cout<<sum<<endl;
+    return 0;
+}
