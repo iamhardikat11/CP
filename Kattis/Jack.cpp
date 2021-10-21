3e2496983aab2f9762d071ebc736552db976b346
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-21 19:54:12 +0530

    Add files via upload

---
+++ Kattis/Jack.cpp
@@ -0,0 +1,10 @@
+//Link:-https://open.kattis.com/problems/jackolanternjuxtaposition
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int N,T,M;
+    cin>>N>>T>>M;
+    cout<<N*T*M<<endl;
+    return 0;
+}
