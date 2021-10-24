a846cb685b3b8f94acfd07468c8811ce4c8e210b
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-24 17:28:31 +0530

    Create 4_Minimum_of_Two.cpp

---
+++ CodeAbbey/4_Minimum_of_Two.cpp
@@ -0,0 +1,16 @@
+//Link:-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<(a>=b?b:a)<<" ";
+    }
+    cout<<"\n";
+    return 0;
+}
