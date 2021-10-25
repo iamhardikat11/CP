a556a983e060666fbfe1b6621c157c5887e33ca2
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-25 17:56:39 +0530

    Create 6_Rounding.cpp

---
+++ CodeAbbey/6_Rounding.cpp
@@ -0,0 +1,33 @@
+//Link:-https://www.codeabbey.com/index/task_view/rounding
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	     double a,b;
+         cin>>a>>b;
+	     double d=a/b;
+         double f=d-(long)d;
+         if(d>=0){
+         if(f>=0.5)
+             cout<<(d-f)+1<<" ";
+         else if(f<0.5)
+              cout<<(d-f)<<" ";
+         }
+         else{
+             f=-f;
+             if(f>=0.5)
+               cout<<(d+f)-1<<" ";
+             else if(f<0.5)
+              cout<<(d+f)<<" ";
+         }
+         
+
+	}
+	return 0;
+}
