490ff850f218733a2543da010dec0789ecb4e602
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-26 12:03:12 +0530

    Update 7_Fahrenheit_to_Celcius.cpp

--- CodeAbbey/7_Fahrenheit_to_Celcius.cpp
+++ CodeAbbey/7_Fahrenheit_to_Celcius.cpp
@@ -1,16 +1,35 @@
 //Link:-https://www.codeabbey.com/index/task_view/fahrenheit-celsius
 #include <iostream>
 using namespace std;
+void round(double d)
+{
+         double f=d-(long)d;
+         if(d>=0){
+         if(f>=0.5)
+             cout<<(d-f)+1<<" ";
+         else if(f<0.5)
+              cout<<(d-f)<<" ";
+         }
+         else{
+             f=-f;
+             if(f>=0.5)
+               cout<<(d+f)-1<<" ";
+             else if(f<0.5)
+              cout<<(d+f)<<" ";
+         }
+         
+}
+
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        n=(5*(n-32)/9);
-        cout<<n<<" ";
-    }
-    return 0;
+     int t;
+     cin>>t;
+     while(t--)
+     {
+          int F;
+          cin>>F;
+          double C=(5.0*((double)F-32.0))/9.0;
+          round(C);
+     }
+     return 0;
 }
