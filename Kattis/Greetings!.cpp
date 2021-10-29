f2adf3c08acb04f84689fbb0e769d5f31e0ae774
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-29 14:45:05 +0530

    Create Greetings!.cpp

---
+++ Kattis/Greetings!.cpp
@@ -0,0 +1,15 @@
+//Link:-https://open.kattis.com/problems/greetings2
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+int main()
+{
+     string s;
+     getline(cin,s);
+     int n=s.length()-2;
+     cout<<"h";
+     for(int i=1;i<=2*n;i++)
+      cout<<"e";
+     cout<<"y";
+     return 0;
+}
