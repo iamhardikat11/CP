fb0c6c2ffec4242e2a71ec40046ba36038c19ac1
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-29 14:35:12 +0530

    Create FYI.cpp

---
+++ Kattis/FYI.cpp
@@ -0,0 +1,13 @@
+//Link:-https://open.kattis.com/problems/fyi
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int a;
+     cin>>a;
+     if(a/10000==555)
+      cout<<1;
+     else
+      cout<<0;
+     return 0;
+}
