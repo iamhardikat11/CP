c7fd6d23e6abda84b1889d43d123f5b7b659a141
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-10-31 10:30:28 +0530

    Create Triangle_Area.java

---
+++ Kattis/Triangle_Area.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Triangle_Area{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(0.5*a*b); 
+    }
+}
