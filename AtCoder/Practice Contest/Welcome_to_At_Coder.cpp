976409f35f55eda1af66012e8b22fe5aa38fb18e
iamhardikat11 <iamhardikat11@gmail.com>
2021-11-07 18:24:46 +0530

    Commit

---
+++ AtCoder/Practice Contest/Welcome_to_At_Coder.cpp
@@ -0,0 +1,11 @@
+//Link:-https://atcoder.jp/contests/practice/tasks/practice_1
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cin>>a>>b>>c;
+  string s;
+  cin>>s;
+  cout<<a+b+c<<" "<<s;
+}
