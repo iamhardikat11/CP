849269734b434cf92a95022877f93ecc3cb8c1ca
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-15 18:30:36 +0530

    Update Lucky_Four.cpp

--- CodeChef/February Lunchtime 2015/Lucky_Four.cpp
+++ CodeChef/February Lunchtime 2015/Lucky_Four.cpp
@@ -1,1 +1,25 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int solve(int N)
+{
+     int count=0;
+     while(N!=0)
+     {
+       if(N%10==4)
+        count++;
+       N = N/10;
+     }
+     return count;
+}
+int main()
+{
+     int T;
+     cin >> T;
+     while(T--)
+     {
+          int N;
+          cin >> N;
+          cout<<solve(N)<<endl;
+     }
+     return 0;
+}
