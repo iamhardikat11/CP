b4155ac515f597139e9faf8963b1e1419bfd8930
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-15 18:17:49 +0530

    Create Smart Phone.cpp

---
+++ CodeChef/Past ZCO Problems/Smart Phone.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+     int N;
+     scanf("%d",&N);
+     std::vector<ll> v;
+     int a;
+     for(int i=0;i<N;i++)
+     {
+          scanf("%lld",&a);
+          v.
+     }
+}
