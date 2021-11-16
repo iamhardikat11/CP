d694f81465196524b4666324c101c252090a5931
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-16 08:33:37 +0530

    Create A_Real_Challenge.java

---
+++ Kattis/A_Real_Challenge.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class A_Real_Challenge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.print(4*Math.sqrt(a));
+    }
+}
