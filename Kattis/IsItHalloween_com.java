9bd9ee2ab9cbe6103f4996100b4b4e008ec153bc
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-19 08:48:07 +0530

    Create IsItHalloween_com.java

---
+++ Kattis/IsItHalloween_com.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class IsItHalloween_com {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        if(s.equals("OCT 31") || s.equals("DEC 25"))
+            System.out.println("yup");
+        else
+            System.out.println("nope");
+    }
+}
