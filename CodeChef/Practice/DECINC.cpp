95bae9d8c74b5cc8008d608fb20b0a947b866ace
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-20 09:34:23 +0530

    Create DECINC.cpp

---
+++ CodeChef/Practice/DECINC.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin >> N;
+	if(N%4==0)
+	{
+	     cout << N+1 << endl;
+	}
+	else
+	{
+	     cout<< N-1 << endl;
+	}
+	return 0;
+}
