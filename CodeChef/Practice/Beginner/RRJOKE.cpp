1198bf7382b875ce37f6754f4a3206f5ebf8f009
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-22 17:22:25 +0530

    Create RRJOKE.cpp

---
+++ CodeChef/Practice/Beginner/RRJOKE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	     int N;
+	     cin >> N;
+	     int arr[N][2];
+	     for(int i=0;i<N;i++)
+	     {
+	          cin >> arr[i][0] >> arr[i][1];
+	     }
+	     
+	}
+	return 0;
+}
