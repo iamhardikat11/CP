277af1a45366093cfdbad4cd9049a001ec3b8a6b
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-23 12:53:09 +0530

    Create CV.cpp

---
+++ CodeChef/Practice/Beginner/CV.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define isconsonant(ch) ( ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve(string s, int N) {
+     int count = 0;
+     for(int i=0;i<N-1;i++)
+     {
+          if(!isconsonant(s[i]) && isconsonant(s[i+1]))
+           count++;
+     }
+     cout << count <<endl;
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+     cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+     //    printf("Case #",t,": "); 
+     int N;
+     cin >> N;
+     string s;
+     cin >> s;
+     solve(s,N);
+    }
+}
