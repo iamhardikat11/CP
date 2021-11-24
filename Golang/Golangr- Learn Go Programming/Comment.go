9e2ff39a2259f78fb86c17ad7687df334aa1005b
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-24 10:47:05 +0530

    Create Comment.go

---
+++ Golang/Golangr- Learn Go Programming/Comment.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main()
+{
+  /* This is a multi-line comment 
+     You can type within this section. */
+  fmt.println("Go!")
+  // This is a Single-Line Comment
+  fmt.println("Lang!")
+}
