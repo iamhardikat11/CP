c749160a11f53db2acea475f30099aa0d7324817
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-25 22:40:19 +0530

    Update Hello_World.java

--- Exercism/Java/Hello_World.java
+++ Exercism/Java/Hello_World.java
@@ -1,7 +1,12 @@
-class Greeter {
-
-    String getGreeting() {
+import java.util.*;
+class Hello_World
+{
+   static class Greeter 
+   {
+     String getGreeting() 
+     {
         return "Hello, World!";
-    }
+     }
+   }
 
 }
