5d8ce6483ec314ffbd24fe4ace8f1ea742928268
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-25 22:24:13 +0530

    Create Problem_39.cpp

---
+++ Daily Coding Problem/Problem_39.cpp
@@ -0,0 +1,67 @@
+//This problem was asked by Dropbox.
+
+/*Conway's Game of Life takes place on an infinite two-dimensional board of square cells. Each cell is either dead or alive, and at each tick, the following rules apply:
+
+Any live cell with less than two live neighbours dies.
+Any live cell with two or three live neighbours remains living.
+Any live cell with more than three live neighbours dies.
+Any dead cell with exactly three live neighbours becomes a live cell.
+A cell neighbours another cell if it is horizontally, vertically, or diagonally adjacent.
+
+Implement Conway's Game of Life. It should be able to be initialized with a starting list of live cell coordinates and the number of steps it should run for. Once initialized, it should print out the board state at each step. Since it's an infinite board, print out only the relevant coordinates, i.e. from the top-leftmost live cell to bottom-rightmost live cell.
+
+You can represent a live cell with an asterisk (*) and a dead cell with a dot (.).
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    
+}
+void input(int arr[][],int N,int M)
+{
+     for(int i=0;i<N;i++)
+     {
+          for(int j=0;j<M;j++)
+           scanf("%c",&arr[i][j]);
+     }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    scanf("%d",&tc); //Number of Test Cases
+    for (int t = 1; t <= tc; t++) {
+        printf("Case #",t,": "); 
+        printf("Enter the Value of M(No. of Columns) and N(No. of Rows) for this Conway's Game of Life:\n");
+        int M,N;
+        scanf("%d %d",&M,&N);
+        char arr[N][M];
+        input(arr,N,M);
+        solve();
+    }
+}
