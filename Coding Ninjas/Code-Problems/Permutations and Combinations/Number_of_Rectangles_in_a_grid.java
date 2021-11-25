c8b8632a186fcb8d77d47409a646dc5fb38083bc
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-25 23:04:54 +0530

    Update Number_of_Rectangles_in_a_grid.java

--- Coding Ninjas/Code-Problems/Permutations and Combinations/Number_of_Rectangles_in_a_grid.java
+++ Coding Ninjas/Code-Problems/Permutations and Combinations/Number_of_Rectangles_in_a_grid.java
@@ -1,11 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class Number_of_Rectangles_in_a_grid {
   
 public static class Solution{
-	public static long countRectangles(int m, int n){
+	public static long countRectangles(int m, int n)
+	{
 		// Write your code here.
-        long prod = 1;
-        prod = ((long)m*(long)(m+1)*(long)(n)*(long)(n+1))/4;
-        return prod;
+          long prod = 1;
+          prod = ((long)m*(long)(m+1)*(long)(n)*(long)(n+1))/4;
+          return prod;
+        }
+	public static void main(String args[])
+	{
+	   Scanner sc = new Scanner(System.in);
+	   int t = sc.nextInt();
+	   while(t-->0)
+	   {
+		   int n = sc.nextInt();
+		   int m = sc.nextInt();
+		   System.out.println("%d\n",countRectangles(m,n));
+	   }
+	}	
   }
-	}
 }
