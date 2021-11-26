8050ee208023525da39eead04f0dcb66f40866ad
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-26 10:11:31 +0530

    Create Find_power_of_a_number.java

---
+++ Coding Ninjas/Code-Problems/Number Theory/Find_power_of_a_number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Find_power_of_a_number
+{
+  class Solution {
+	
+	 public static void main(String args[]) {
+		
+		// Write code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((int)Math.pow(x,n));
+   }
+	}
+}
