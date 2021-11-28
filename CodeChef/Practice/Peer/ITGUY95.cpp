720da5c92fbb1c360f897d4b5af14083d031d77c
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-28 17:23:04 +0530

    Create ITGUY95.cpp

---
+++ CodeChef/Practice/Peer/ITGUY95.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(int N) {
+     for(int i=1;i<=N;i++)
+     {
+          for(int j=1;j<=N-i;j++)
+           printf(" ");
+          for(int j=1;j<=i;j++)
+           printf("(");
+          printf("*");
+          for(int j=1;j<=i;j++)
+           printf(")");
+          for(int j=1;j<=N-i;j++)
+           printf(" ");
+          printf("\n");
+     }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    scanf("%d",&tc);
+    for (int t = 1; t <= tc; t++) {
+     //    printf("Case #",t,": "); 
+    int N;
+    scanf("%d",&N);
+    solve(N);
+    }
+}
