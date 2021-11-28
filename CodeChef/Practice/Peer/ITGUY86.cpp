bc526314a6ae7d91632912913a195bcc4a48594c
iamhardikat11 <iamhardikat11@gmail.com>
2021-11-28 17:48:50 +0530

    Commit

---
+++ CodeChef/Practice/Peer/ITGUY86.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    scanf("%d",&tc);
+    for (int t = 1; t <= tc; t++) 
+    {
+     //    printf("Case #",t,": "); 
+     int N;
+     scanf("%d",&N);
+     for(int i=1;i<=N;i++)
+     {
+          printf(".");
+          for(int j=1;j<=i;j++)
+           printf("%d",i);
+          printf("\n");
+     }
+    }
+}
