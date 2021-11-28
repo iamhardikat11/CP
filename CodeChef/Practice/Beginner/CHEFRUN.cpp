d7b79e49534b73cd7d3d3caa3f846d9a9a186733
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-28 21:15:59 +0530

    Update CHEFRUN.cpp

--- CodeChef/Practice/Beginner/CHEFRUN.cpp
+++ CodeChef/Practice/Beginner/CHEFRUN.cpp
@@ -1,23 +1,51 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
-string solve(int X1, int X2, int X3, int V1, int V2)
-{
-     if(((float)abs(X3-X1)/(float)V1)<((float)abs(X3-X2)/(float)V2))
-      return "Chef";
-     else if(((float)abs(X3-X1)/(float)V1)>((float)abs(X3-X2)/(float)V2))
-      return "Kefa";
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve(int X1,int X2,int X3,int V1,int V2) {
+     double t1 = ((double)(X3-X1))/V1;
+     double t2 = ((double)(X2-X3))/V2;
+     if(t1<t2)
+      printf("Chef\n");
+     else if(t2<t1)
+      printf("Kefa\n");
      else
-      return "Draw";
+      printf("Draw\n");
+     
+    
 }
+
 int main() {
-	// your code goes here
-	int t;
-	cin >> t;
-	while(t--)
-	{
-	     int X1,X2,X3,V1,V2;
-	     cin >> X1 >> X2 >> X3 >> V1 >> V2;
-	     cout << solve(X1, X2, X3, V1, V2)<<endl;
-	}
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    scanf("%d",&tc);
+    for (int t = 1; t <= tc; t++) {
+        int X1,X2,X3,V1,V2;
+        scanf("%d%d%d%d%d",&X1,&X2,&X3,&V1,&V2);
+        solve(X1,X2,X3,V1,V2);
+    }
 }
