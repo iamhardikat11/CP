dfd2ccac196d053acec5da808ce63566cb0f75c1
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-28 21:42:51 +0530

    Create SOCKS1.cpp

---
+++ CodeChef/Practice/Beginner/SOCKS1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int A,B,C;
+    cin >> A >> B >> C;
+    if(A==B || B==C || C==A)
+     printf("YES\n");
+    else
+     printf("NO\n");
+}
