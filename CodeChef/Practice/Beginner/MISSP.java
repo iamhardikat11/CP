bd3c33d5f89ebd1ceba8c9f334d8e4cc5f7612ee
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-29 13:47:22 +0530

    Update MISSP.java

--- CodeChef/Practice/Beginner/MISSP.java
+++ CodeChef/Practice/Beginner/MISSP.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+public class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int N = sc.nextInt();
+            StringBuilder s = new StringBuilder("0".repeat((N-1)/2+1));
+            for(int i = 1;i<=N;i++)
+            {
+                int a = sc.nextInt();
+                s.setCharAt(a-1, ((char)(Integer.parseInt(s.toString().substring(a-1,a))+49)));
+            }
+           System.out.println(s.toString().indexOf("1")+1);
+        }
+    }
+}
