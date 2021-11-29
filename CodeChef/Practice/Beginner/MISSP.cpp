bcdd4a3ce9fca4efb84dedd0e870add6000f5c2d
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-29 13:47:58 +0530

    Update MISSP.cpp

--- CodeChef/Practice/Beginner/MISSP.cpp
+++ CodeChef/Practice/Beginner/MISSP.cpp
@@ -1,1 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+void solve(int N,ll arr[])
+{
+     for(int i=0;i<N;i++)
+     {
+          bool flag = true;
+          for(int j=0;j<N;j++)
+          {
+               if(j!=i && arr[i]==arr[j])
+               {
+                    flag = false;
+                    break;
+               }
+          }
+          if(flag)
+          {
+               printf("%lld\n",arr[i]);
+               break;
+          }
+     }
+}
 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    scanf("%d",&tc);
+    for (int t = 1; t <= tc; t++) 
+    {
+        ll N;
+        scanf("%lld",&N);
+        ll arr[N];
+        for(int i=0;i<N;i++)
+         scanf("%lld",&arr[i]);
+        solve(N,arr);
+    }
+}
