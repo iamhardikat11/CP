b258673ba4d4cf26d140099b86683a877763e41d
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-11-29 23:10:27 +0530

    Create PRF02LOOP02.cpp

---
+++ SPOJ/Basics/PRF02LOOP02.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    cout << "alphabet" << endl;
+    cout <<"
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z" <<endl;
+     return 0;
+}
