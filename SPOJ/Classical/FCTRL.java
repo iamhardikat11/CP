0e6afeb66202c158f338101309dd4e138ced8e9c
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-02 20:29:45 +0530

    Create FCTRL.java

---
+++ SPOJ/Classical/FCTRL.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.*;
+public class FCTRL2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int N = sc.nextInt();
+            BigInteger n = new BigInteger("1");
+            for(int i=2;i<=N;i++)
+                n = n.multiply(new BigInteger(Integer.toString(i)));
+            System.out.println(n.toString());
+        }
+    }
+}
