948d6f11f161a45c3c9a0da96fa2b26261e79a85
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-05 18:08:34 +0530

    Create SPTTRN2.cpp

---
+++ SPOJ/Basics/SPTTRN2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(int N)
+{
+  for(int i=1;i<=N;i++)
+  {
+    for(int j=1;j<=N;j++)
+    {
+      
+    }
+  }
+}
+int main(void)
+{
+  int tc;
+  scanf("%d",&tc);
+  while(tc--)
+  {
+    int N;
+    scnaf("%d",&N);
+    solve(N);
+  }
+}
