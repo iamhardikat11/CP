5e76fadd2f9993bef98cfde33cd6ea33214fb5d2
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-06 08:43:33 +0530

    Create ITGUY77.cpp

---
+++ CodeChef/Practice/Peer/ITGUY77.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int t;
+     scanf("%d",&t);
+     while(t--)
+     {
+          int N;
+          scanf("%d",&N);
+          for(int i=1;i<=N;i++)
+          {
+               for(int j=1;j<=N-i;j++)
+                printf(" ");
+               for(int j=1;j<=2*i-1;j++)
+               {
+                    if(i%2==0)
+                    {
+                      printf("%c",j%2==0?'A':'*');   
+                    }
+                    else
+                    {
+                      printf("%c",j%2==0?'*':'A');   
+                    }
+               }
+               printf("\n");
+          }
+     }
+}
