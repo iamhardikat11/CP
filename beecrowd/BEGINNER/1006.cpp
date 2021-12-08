48f14736808e3c12204b92d0783d37b9d3838eaf
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 15:48:03 +0530

    Create 1006.cpp

---
+++ beecrowd/BEGINNER/1006.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    float a,b,c;
+    scanf("%f%f%f",&a,&b,&c);
+    printf("MEDIA = %.1f\n",(0.2*a+0.3*b+0.5*c));
+    return 0;
+}
