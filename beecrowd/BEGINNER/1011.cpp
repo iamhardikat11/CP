5b3988e0d58ded24f4bc737577c06c78c8ce1ead
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 16:04:58 +0530

    Create 1011.cpp

---
+++ beecrowd/BEGINNER/1011.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    double a;
+    cin >> a;
+    printf("VOLUME = %.3lf\n",4.0/3*3.14159*R*R*R);
+    return 0;
+}
