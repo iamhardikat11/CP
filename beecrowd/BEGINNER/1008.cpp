8a84529bc561d71c10839ac582cd309f35500d24
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 15:51:53 +0530

    Create 1008.cpp

---
+++ beecrowd/BEGINNER/1008.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a,b;
+    double c;
+    scanf("%d%d%lf,&a,&b,&c");
+    printf("NUMBER = %d\n",a);
+    printf("SALARY = U$ %.2lf\n",(b*c));
+    return 0;
+}
