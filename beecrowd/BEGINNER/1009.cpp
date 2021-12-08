594863f7d6cb35b47889da646756bdcb6f1a92d5
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 15:59:11 +0530

    Create 1009.cpp

---
+++ beecrowd/BEGINNER/1009.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    string s;
+    double a,b;
+    cin >> s >> a >> b;
+    printf("TOTAL = R$ %.2lf\n",(a+0.15*b));
+    return 0;
+}
