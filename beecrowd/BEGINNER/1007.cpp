b60811618f2d72386d4159d7bd92c70eeaf49875
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 15:49:15 +0530

    Create 1007.cpp

---
+++ beecrowd/BEGINNER/1007.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    printf("DIFERENCA = %d\n",(a*b-c*d));
+    return 0;
+}
