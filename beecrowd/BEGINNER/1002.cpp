bb41f05bcd3b39d806fed04226805678ac19bf3f
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 15:36:54 +0530

    Create 1002.cpp

---
+++ beecrowd/BEGINNER/1002.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    double R;
+    scanf("%lf",&R);
+    printf("A=.4%lf\n",(3.14159*pow(R,2)));
+    return 0;
+}
