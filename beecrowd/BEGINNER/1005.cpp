4b1f3106a19eef826594a6cff4775ec941980544
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 15:42:31 +0530

    Create 1005.cpp

---
+++ beecrowd/BEGINNER/1005.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    float A,B;
+    scanf("%f%f",&A,&B);
+    printf("MEDIA = %f\n",(A+B)/2.0);
+    return 0;
+}
