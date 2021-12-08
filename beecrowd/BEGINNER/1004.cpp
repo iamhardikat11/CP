1fa6c10109ed95444f00bcafe6d2edcfdb6c3dae
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 15:41:20 +0530

    Create 1004.cpp

---
+++ beecrowd/BEGINNER/1004.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int A,B;
+    scanf("%d%d",&A,&B);
+    printf("PROD = %d\n",(A*B));
+    return 0;
+}
