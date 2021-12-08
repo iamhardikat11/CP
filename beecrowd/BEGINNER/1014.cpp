11799971e86ce549a5a968f5d1e1597ab13d781c
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-08 17:42:34 +0530

    Create 1014.cpp

---
+++ beecrowd/BEGINNER/1014.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a;
+    double b;
+    cin >> a >> b;
+    printf("%.3lf km/l\n",a/b);
+    return 0;
+}
