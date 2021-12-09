ecc367062578c9e69056ca1297714fda689497be
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-09 22:07:43 +0530

    Create 1020.cpp

---
+++ beecrowd/BEGINNER/1020.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int N;
+    scanf("%d",&N);
+    int a = N/365;
+    N = N%365;
+    int b = N/30;
+    N = N%30;
+    printf("%d  ano(s)\n%d mes(es)\n%d dia(s)\n",a,b,N);
+    return 0;
+}
