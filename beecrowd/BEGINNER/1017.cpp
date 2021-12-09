3de76ee26412bcd5a8b4eec87b4a0751760b8413
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-09 12:26:03 +0530

    Create 1017.cpp

---
+++ beecrowd/BEGINNER/1017.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a,b;
+    cin >> a >> b;
+    printf("%.3lf\n",(a*b/12.0));
+    return 0;
+}
