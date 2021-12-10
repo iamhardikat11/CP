84970b0c26701ff35ee603819cdbf4f44ff68af9
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-10 18:33:52 +0530

    Create Problem_47.cpp

---
+++ Daily Coding Problem/Problem_47.cpp
@@ -0,0 +1,28 @@
+/**
+*This problem was asked by Facebook.
+
+*Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
+
+*For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+int main()
+{
+  int N;
+  cin >> N;
+  int arr[N];
+  for(int i=0;i<N;i++) cin >> arr[i];
+  int max = 0;
+  for(int i=0;i<N;i++)
+  {
+    for(int j=i+1;j<N;j++)
+      if(arr[j]-arr[i]>max)
+        max = arr[j]-arr[i];
+  }
+  cout << max <<endl;
+  return 0;
+}
