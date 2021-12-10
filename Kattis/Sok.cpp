1802f565d973dbfbb1036d01629c01ed69056818
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-10 13:49:16 +0530

    Update Sok.cpp

--- Kattis/Sok.cpp
+++ Kattis/Sok.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
-int main()
-{
-    int A,B,C,I,J,K;
-    cin >> A >> B >> C >> I >> J >> K;
-    
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a[3];
+    int b[3];
+    for(int i=0;i<3;i++)
+        cin >> a[i];
+    for(int j=0;j<3;j++)
+        cin >> b[j];
+    double quan = (double)a[0]/b[0];
+    int index = 0;
+    for(int i=1;i<=2;i++)
+    {
+        if(((double)a[i]/b[i])<quan)
+        {
+            quan = ((double)a[i]/b[i]);
+            index = i;
+        }
+    }
+    printf("%.6lf %.6lf %.6lf\n",(double)a[0]-b[0]*quan,(double)a[1]-b[1]*quan,(double)a[2]-b[2]*quan);
+    return 0;
 }
