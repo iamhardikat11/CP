31d259485687e60651c1833ca9f9e045a78e271e
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-10 19:31:13 +0530

    Add files via upload

--- Kattis/Building_Pyramids.java
+++ Kattis/Building_Pyramids.java
@@ -1,25 +1,20 @@
 import java.util.*;
-import java.lang.Math.*;
-public class Building_Pyramids
-{
+import java.io.*;
+import java.lang.*;
+
+import static java.lang.Math.pow;
+
+public class Building_Pyramids {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        double i=1;
-        if(N==1)
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while(n>=(4*pow(i,2)-4*i+1))
         {
-          System.out.println(1);
-          return;
+           n-=(4*pow(i,2)-4*i+1);
+           i++;
         }
-        while(true)
-        {
-            int M =  ((int)(4*Math.pow(i,3)-i))/3;
-            if(M>=N)
-             break;
-            i++;
-        }
-        System.out.println((int)(i-1));
-        in.close();
+        System.out.println(i-1);
     }
 }
