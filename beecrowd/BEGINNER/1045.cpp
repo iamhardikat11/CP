f356af59489566615749c06c261296492dabd354
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-11 22:13:15 +0530

    Update 1045.cpp

--- beecrowd/BEGINNER/1045.cpp
+++ beecrowd/BEGINNER/1045.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
- 
+
 int main() {
- 
+
     /**
      * Escreva a sua solução aqui
      * Code your solution here
      * Escriba su solución aquí
      */
-    double a,b,c;
-    cin >> a >> b >> c;
-    if(a>=b+c)
-     printf("NAO FORMA TRIANGULO\n");
-    if(pow(a,2)==pow(b,2)+pow(c,2))
-     printf("TRIANGULO RETANGULO\n");
-    if(pow(a,2)>pow(b,2)+pow(c,2))
-     printf("TRIANGULO OBTUSANGULO\n");
-    if(pow(a,2)<pow(b,2)+pow(c,2))
-     printf("TRIANGULO ACUTANGULO\n");
-    if(a==b && b==c)
-     printf("TRIANGULO EQUILATERO\n");
-    if((a==b && a!=c)|| (b==c && a!=c) || (c==a && b!=a))
-     printf("TRIANGULO ISOSCELES\n");
+    double arr[3];
+    cin >> arr[0] >> arr[1] >> arr[2];
+    sort(arr,arr+3);
+    double a = arr[2],b = arr[1],c = arr[0];
+    if(a>=(b+c))
+        printf("NAO FORMA TRIANGULO\n");
+    else {
+        if (pow(a, 2) == (pow(b, 2) + pow(c, 2)))
+            printf("TRIANGULO RETANGULO\n");
+        if (pow(a, 2) > pow(b, 2) + pow(c, 2))
+            printf("TRIANGULO OBTUSANGULO\n");
+        if (pow(a, 2) < pow(b, 2) + pow(c, 2))
+            printf("TRIANGULO ACUTANGULO\n");
+        if (a == b && b == c)
+            printf("TRIANGULO EQUILATERO\n");
+        if ((a == b && a != c) || (b == c && a != c) || (c == a && a != b))
+            printf("TRIANGULO ISOSCELES\n");
+    }
     return 0;
 }
