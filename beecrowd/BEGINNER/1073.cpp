7cd274f7ac65241629dc62c0c590eab572acb4d7
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-12 00:22:30 +0530

    Create 1073.cpp

---
+++ beecrowd/BEGINNER/1073.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int N;
+    cin >> N;
+    for(int i=2;i<=N;i+=2)
+     printf("%d^2 = %d\n",i,pow(i,2));
+    return 0;
+}
