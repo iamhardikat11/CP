5f4a4dd66d2e6de1ccdb03b0075bca5aa9230ceb
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-12 00:38:51 +0530

    Create 1067.cpp

---
+++ beecrowd/BEGINNER/1067.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int X;
+    cin >> X;
+    for(int i=1;i<=X;i+=2)
+     printf("%d\n",i);
+    return 0;
+}
