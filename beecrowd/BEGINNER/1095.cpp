1af7867113f63a35fb4792e53a410f64e325232b
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-12 17:18:24 +0530

    Create 1095.cpp

---
+++ beecrowd/BEGINNER/1095.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    for(int i=1;i<=13;i++)
+    {
+        printf("I=%d J=%d\n",(1+(i-1)*3),(60-(i-1)*5));
+    }
+    return 0;
+}
