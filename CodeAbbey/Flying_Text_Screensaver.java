dc48ea3530f29163b4d707690bdeddd187baffd1
iamhardikat11 <iamhardikat11@gmail.com>
2021-12-12 15:57:21 +0530

    Commit

---
+++ CodeAbbey/Flying_Text_Screensaver.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Flying_Text_Screensaver
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int w,h,l;
+        w = in.nextInt();
+        h = in.nextInt();
+        l  = in.nextInt();
+        System.out.print("0 0");
+        
+    }
+}
