27a41cf961d0b922a0de79d2e0b5259b73ff203a
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-12 15:22:12 +0530

    Create 1078.cpp

---
+++ beecrowd/BEGINNER/1078.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int X;
+    cin >> X;
+    for(int i=1;i<=10;i++)
+     printf("%d x %d = %d\n",i,X,i*X);
+    return 0;
+    
+}
