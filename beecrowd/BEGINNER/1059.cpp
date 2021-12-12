505691d688fa04d6dfa1a9ab9010b96903016421
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-12 14:44:31 +0530

    Update 1059.cpp

--- beecrowd/BEGINNER/1059.cpp
+++ beecrowd/BEGINNER/1059.cpp
@@ -9,7 +9,7 @@ int main() {
      * Code your solution here
      * Escriba su solución aquí
      */
-    for(int i=2;i<=100;i++)
+    for(int i=2;i<=100;i+=2)
      printf("%d\n",i);
     return 0;
 }
