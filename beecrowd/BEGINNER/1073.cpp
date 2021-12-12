65b3a22f4c42043bfb3cb7b937465818515bdd34
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-12 15:05:15 +0530

    Update 1073.cpp

--- beecrowd/BEGINNER/1073.cpp
+++ beecrowd/BEGINNER/1073.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
 int main() {
@@ -12,6 +12,8 @@ int main() {
     int N;
     cin >> N;
     for(int i=2;i<=N;i+=2)
-     printf("%d^2 = %6d\n",i,(int)pow(i,2));
+    {
+        printf("%d^2 = %d\n",(i),i*i);
+    }
     return 0;
 }
