5388fd30f409e0f02414c111c4ff509df36b2e9b
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-17 17:53:46 +0530

    Create 1864.cpp

---
+++ beecrowd/BEGINNER/1864.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+//#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int N;
+    cin >> N;
+    string s = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    cout << s.substr(0,N) << endl;
+    return 0;
+}
