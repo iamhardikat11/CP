52c3603f99ccddfaa96b764f85e125b0bb1ddd38
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2021-12-17 18:42:24 +0530

    Update Part_1.java

--- Advent of Code/2017/Day 1/Part_1.java
+++ Advent of Code/2017/Day 1/Part_1.java
@@ -1,1 +1,19 @@
-
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int sum = 0;
+        for(int i=0;i<=s.length()-2;i++)
+        {
+           if(s.charAt(i)==s.charAt(i+1))
+               sum += (s.charAt(i)-48);
+        }
+        if(s.charAt(s.length()-1)==s.charAt(0))
+            sum += (s.charAt(0)-48);
+        System.out.println(sum);
+    }
+}
