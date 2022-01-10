1fe35f209c4e12ecee841898f6dca8862bf8e75d
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-01-10 23:54:11 +0530

    Create main.cpp

---
+++ Daily Coding Problem/Problem 85/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int ret()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int x,y,b;
+  cin >> x >> y >> b;
+  if(x&1)
+    return x;
+  else
+    return y;
+}
