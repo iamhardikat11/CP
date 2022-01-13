33af9548a00afb07e5b835de0362a8555087a822
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-01-13 23:15:40 +0530

    Update Problem_1.java

--- Daily Coding Problem/Problem 1/Problem_1.java
+++ Daily Coding Problem/Problem 1/Problem_1.java
@@ -5,6 +5,32 @@ public class Main
 {
   public static void main(String[] args)
   {
-    
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++)
+      arr[i] = sc.nextInt();
+    Arrays.sort(arr);
+    int lt = 0;
+    int rt = arr.length-1;
+    while(lt<rt)
+    {
+      if(arr[lt]+arr[rt]==k)
+      {
+        System.out.println("YES");
+        break;
+      }
+      else if(arr[lt]+arr[rt]>k)
+      {
+        rt--;
+      }
+      else
+      {
+        lt++;
+      }
+    }
+    System.out.println("NO");
+    return 0;
   }
  }
