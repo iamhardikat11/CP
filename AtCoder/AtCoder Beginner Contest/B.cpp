6eea4a5f90f6df32ff977833a4d6a742086e2629
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-01-26 00:23:51 +0530

    Create B.cpp

---
+++ AtCoder/AtCoder Beginner Contest/B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int D,N;
+	cin >> D >> N;
+	cout << pow(100,D)*N << endl;
+	return 0;
+}
