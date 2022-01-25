d21984d3c5b390a95c74c6116e76c36339445f7c
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-01-26 01:01:29 +0530

    Create A.cpp

---
+++ AtCoder/AtCoder Beginner Contest 099/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int N;
+	cin >> N;
+	if(N<1000)
+		cout << "ABC" << endl;
+	else
+		cout << "ABD" << endl;
+}
