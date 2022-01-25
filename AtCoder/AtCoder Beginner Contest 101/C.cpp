2d1a90c16f2a105f3fb59b79d4bac29c9ef7b939
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-01-25 10:31:13 +0530

    Update C.cpp

--- AtCoder/AtCoder Beginner Contest 101/C.cpp
+++ AtCoder/AtCoder Beginner Contest 101/C.cpp
@@ -1,1 +1,11 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n,k;
+  cin >> n >> k;
+  vector<int> v(n);
+  for(int i=0;i<n;i++) cin >> v[i];
+  cout << ceil((double)(n-1)/(k-1)) << endl;
+  return 0;
+}
