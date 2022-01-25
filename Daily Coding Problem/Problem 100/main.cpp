a00357a1d32ede0326135af7134c915fa3072051
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-01-26 02:14:51 +0530

    Create main.cpp

---
+++ Daily Coding Problem/Problem 100/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int n;
+  cin >> n;
+  vector<vector<int> > v(n,vector<int> (2));
+  int cnt = 0;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<2;j++)
+    {
+      cin >> v[i][j];
+    }
+  }
+  for(int i=1;i<n;i++)
+  {
+    
+  }
+}
