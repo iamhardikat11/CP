316a5d71c5c29c9ed3ed44746fc7f2de36ba0a1d
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-01-28 02:20:22 +0530

    Create main.cpp

---
+++ Daily Coding Problem/Problem 101/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isPrime(int n)
+{
+  if(i==0 || i==1) return false;
+  if(i==2) return true;
+  for(int i=2;i<=sqrt(n);i++)
+  {
+    if(n%i==0)
+      return false;
+  }
+  return true;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int n;
+  cin >> n;
+  int i = -1;
+  for(int i=3;i<n;i++)
+  {
+    if(isPrime(i))
+    {
+      if(isPrime(n-i))
+      {
+        break;
+      }
+    }
+  }
+  cout << (i) << (n-i) << endl;
+  return 0;
+}
