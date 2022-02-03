4af227921ed0796d1be56d4edee1be1aaf5aba49
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-02-04 04:37:14 +0530

    Create main.cpp

---
+++ Daily Coding Problem/Problem 107/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int value;
+  struct Node* left;
+  struct Node* right;
+};
+typedef struct Node TreeNode;
+
+void insertNode(Treenode tree, int val)
+{
+  if()
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  TreeNode tree;
+  
+}
