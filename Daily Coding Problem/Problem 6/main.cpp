958d49f459b17c05e2b08256ec260fc79045b60b
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-02-04 06:15:05 +0530

    Create main.cpp

---
+++ Daily Coding Problem/Problem 6/main.cpp
@@ -0,0 +1,19 @@
+// C++ Implementation of a XOR Linked List........
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int value;
+  struct Node* xnode;
+};
+
+
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+}
