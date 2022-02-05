1422e921b99e80b0ff498a4997ef964f2bc7ed40
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-02-05 21:11:36 +0530

    Create B.cpp

---
+++ AtCoder/Monoxer Programming Contest 2022（AtCoder Beginner Contest 238/B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for(int i=0;i<N;i++) cin >> A[i];
+  vector<int> angles;
+  angles.push_back(0);
+  for(int i=0;i<N;i++)
+  {
+    for(int j=0;j<angles.size();j++)
+    {
+      angles[j]+=A[i];
+    }
+    angles.push_back(0);
+  }
+  sort(angles.begin(),angles.end());
+  int max_= INT_MIN;
+  for(int i=0;i<angles.size();i++)
+  {
+    if(i==angles.size()-1)
+    {
+      if(360-angles[i]>max_)
+        max_ = 360-angles[i];
+    }
+    else
+    {
+      if((angles[i+1]-angles[i])>max_)
+        max_ = angles[i+1]-angles[i];
+    }
+  }
+  cout << max_ << endl;
+  return 0;
+}
