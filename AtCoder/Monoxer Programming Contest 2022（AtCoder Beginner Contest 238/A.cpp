f0204dbbf767b9917c8131dbf60b68613da52812
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-02-05 21:05:14 +0530

    Create A.cpp

---
+++ AtCoder/Monoxer Programming Contest 2022（AtCoder Beginner Contest 238/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int N;
+  cin >> N;
+  if(N==2 || N==3 || N==4) cout << "No" << endl;
+  else cout << "Yes" << endl;
+  return 0;
+}
