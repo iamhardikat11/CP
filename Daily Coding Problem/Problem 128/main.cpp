6c570fa38704c6755e9dae5b9d7c55974ab7e997
Hardik Soni <79035082+iamhardikat11@users.noreply.github.com>
2022-02-24 06:53:25 +0530

    Create main.cpp

---
+++ Daily Coding Problem/Problem 128/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespacet std;
+void towerOfHanoi(int a,int b,int c)
+{
+  if(a==0)
+    return;
+  else if(b==0 && a>c)
+  {
+    cout << "Move "
+  }
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int n;
+  cin >> n;
+  towerOfHanoi(n,0,0);
+}
