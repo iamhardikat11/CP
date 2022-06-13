30e9715f6aa081896a24ef1cb5e22c62ddc5e886
iamhardikat11 <iamhardikat11@gmail.com>
2022-06-13 10:56:27 +0530

    Commit

---
+++ AtCoder/AtCoder Beginner Contest 095/C_-_Half_and_Half.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int A,B,C,X,Y;
+    cin >> A >> B >> C >> X >> Y;
+    
+}
---
+++ AtCoder/AtCoder Beginner Contest 095/D_-_Static_Sushi.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll N,C;
+    cin >> N >> C;
+    vector<pair<ll,ll> > A(N);
+    for(int i = 0; i < N; i++) 
+        cin >> A[i].first >> A[i].second;
+    
+    return 0;
+}
