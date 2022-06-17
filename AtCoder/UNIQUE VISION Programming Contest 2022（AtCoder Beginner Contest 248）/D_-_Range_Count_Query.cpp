325bb625f08c594d14bd7fd81b9f76c44e056ba0
iamhardikat11 <iamhardikat11@gmail.com>
2022-06-17 21:03:49 +0530

    Commit

--- AtCoder/UNIQUE VISION Programming Contest 2022（AtCoder Beginner Contest 248）/D_-_Range_Count_Query.cpp
+++ AtCoder/UNIQUE VISION Programming Contest 2022（AtCoder Beginner Contest 248）/D_-_Range_Count_Query.cpp
@@ -10,6 +10,7 @@ int main()
     cin >> Q;
     vector<map<int,int>> v;
     map<int,int> b;
+    v.push_back(b);
     for(int i=0;i<N;i++)
     {
         b[A[i]]++;
@@ -19,6 +20,7 @@ int main()
     {
         int L,R,X;
         cin >> L >> R >> X;
-        
+        cout << v[R-1][X]-v[L-1][X] << endl;
     }
+    return 0;
 }
