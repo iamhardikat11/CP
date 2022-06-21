bedb41c0a462441e93b6ee67d7c73911379d02d6
iamhardikat11 <iamhardikat11@gmail.com>
2022-06-21 20:51:07 +0530

    Commit

---
+++ Codeforces/Codeforces Round #109 (Div. 2)/A_I_love_username.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n;i++) cin >> v[i];
+    int mx = v[0];
+    int mn = v[0];
+    int cnt=0;
+    for(int i=0;i<n;i++)
+    {
+        if(v[i] > mx) {
+            mx = v[i];
+            cnt++;
+        }
+        if(v[i] < mn) {
+            mn = v[i];
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
---
+++ Codeforces/Codeforces Round #661 (Div. 3)/A_Remove_Smallest.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        sort(v.begin(), v.end());
+        int f = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (v[i] - v[i - 1] == 0 || v[i] - v[i - 1] == 1)
+                continue;
+            else
+            {
+                cout << "NO" << endl;
+                f = 0;
+                break;
+            }
+        }
+        if (f)
+            cout << "YES" << endl;
+    }
+    return 0;
+}
