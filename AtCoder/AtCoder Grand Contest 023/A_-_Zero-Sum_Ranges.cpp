fbdc4420921fac161fcc61da31efbc66bedcfa16
iamhardikat11 <iamhardikat11@gmail.com>
2022-07-06 22:40:29 +0530

    Commit

---
+++ AtCoder/AtCoder Grand Contest 023/A_-_Zero-Sum_Ranges.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    int cnt = 0;
+    int temp = 0;
+    int curr = 0;
+    for(int i = 0; i < N; i++)
+    {
+        if(temp==0 && curr!=0)
+        {
+            cnt++;
+            curr = 0;
+        }
+        else
+        {
+            curr++;
+            temp+=a[i];
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+} 
