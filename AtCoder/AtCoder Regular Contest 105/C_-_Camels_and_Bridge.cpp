77d78eab146e44f9e1e03d781aa384a59a0c2770
iamhardikat11 <iamhardikat11@gmail.com>
2022-07-15 17:34:26 +0530

    Commit

--- AtCoder/AtCoder Regular Contest 105/C_-_Camels_and_Bridge.cpp
+++ AtCoder/AtCoder Regular Contest 105/C_-_Camels_and_Bridge.cpp
@@ -16,15 +16,20 @@ const int MAX_N = 1e5 + 5;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
-
+bool cmp(const pair<int,int> a, const pair<int,int> b)
+{
+    return (a.second > b.second || a.first > b.first);
+}
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int N,M;
     cin >> N >> M;
     vector<int> w(N);
+    sort(w.begin(), w.end());
     vector<pair<int,int>> v(M);
     for(int i=0; i<M; i++) cin >> v[i].first >> v[i].second;
+    sort(v.begin(), v.end(), cmp);
     
     return 0;
 }
